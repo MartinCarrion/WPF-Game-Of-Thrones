a1f3abd5b3d6a0e38a592b9492f9badfdaba4e8b
Carrion, Martin A <Martin.A.Carrion-1@ou.edu>
2019-02-25 11:47:09 -0600

    JSON EXAMPLE

---
+++ GameOfThrones02252019/GameOfThrones02252019/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿//MARTIN CARRION!!!!!!!
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfThrones02252019
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //https://got-quotes.herokuapp.com/quotes
+           using(HttpClient client=new HttpClient())
+            {
+                var response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    var Content = response.Content.ReadAsStringAsync().Result;
+                    Quote q = JsonConvert.DeserializeObject<Quote>(Content);
+                    txtQuote.Inlines.Add(new Italic(new Run("Italic")));
+
+                }
+            }
+
+
+
+
+        }
+
+        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
